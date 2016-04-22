e7c2de9838a77a21508e7ff84b133a47264d2469
Noah Butler <NoahButler@Stephens-iMac-2.local>
2016-04-22 17:15:10 -0500

    started work on server code for adding groups

--- Users/views.py
+++ Users/views.py
@@ -4,9 +4,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from Users import utils
-from Users.models import UserData
+from Users.models import UserData, Groups
 
-
+''' This method is responsible for handling the logging of users.
+ After it does that, it sends the user to their home at GolfHub. '''
 def login_page(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -30,6 +31,7 @@ def login_page(request):
     else:
         return render(request, "Users/LoginScreen.html")
 
+''' This method is responsible for handling the signing up of users. '''
 def signup_page(request):
     if request.method == 'POST':
         # create our user
@@ -45,8 +47,24 @@ def signup_page(request):
         # create our user data entry for our user we just created
         userdata = UserData(user=user)
         userdata.save()
-        # TODO: Check for error in sign up
+        # TODO: Check for user errors in sign up
         return render(request, "Users/SuccessfulSignUp.html")
 
     else:
         return render(request, "Users/SignupScreen.html")
+
+
+'''This method is responsible for handling users creating new groups. '''
+def create_new_group(request):
+    # Items Needed: members, creator/owner, group name
+
+    # get our json package
+    data = request.POST['json_data']
+    name = data['name']
+    members = data['members']
+    # create our member csv string for storage
+    members_str = ""
+    for member in members:
+        members_str += member + ","
+
+    group = Groups(name=name, members=members_str)
